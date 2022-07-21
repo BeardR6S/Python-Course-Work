37af5a5a428a9a56e9ac7ca137a5db8fe8f25a06
BeardR6S <brandon.allan.hale@gmail.com>
2022-07-21 19:28:31 -0400

    CP3 update.

---
+++ M2C3/CP3.py
@@ -0,0 +1,74 @@
+#a = "Im so tired"
+# ^String
+
+#import random
+
+#print (random.randrange(1, 50))
+# #^random
+
+#mylist = ["a", "b", "c"]
+# print(mylist)
+# #^list
+
+# print(99 > 9)
+# #^Boolean
+
+#--------------------------------------------------------------------------------------------
+
+# print(a[0:3])
+
+#--------------------------------------
+
+#print(mylist[0])
+
+#--------------------------------------
+
+# x = 11 
+# y = 10 
+
+# print(x + y)
+
+#----------------------------------------------
+
+#print(mylist[-1:])
+
+#-------------------------------------------------
+
+# names = 'harry, alex, susie, jared, gail, conner'
+
+# names_given = names.split(', ')
+
+# print(names_given)
+
+#-------------------------------------------------
+
+# a = "im so tired".upper()
+
+# print(a[0:2])
+
+# a_cap = 'IM'
+
+# b_low = ' so tired'
+
+# print(a_cap + b_low)
+
+#OR 
+
+# b_low = ' so tired'.capitalize()
+
+# print(a_cap + b_low)
+
+#----------------------------------------------
+
+
+# name = 'John'
+
+# age = "21"
+
+# task = f"{name} is {age} years old."
+
+# print(task)
+
+#----------------------------------------------
+
+print( 'Hello World')
