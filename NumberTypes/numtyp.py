fa3236a0a63ab9303ab663f95b702bd518469d8e
BeardR6S <brandon.allan.hale@gmail.com>
2022-07-23 17:00:10 -0400

    new file

---
+++ NumberTypes/numtyp.py
@@ -0,0 +1,10 @@
+#numbers in variables dont need "", that makes them a string.
+
+product_id = 123 #integer data type/Whole Number
+
+sale_price = 14.99 #floating point data type/FLOAT, NOT A DECIMAL. Not as specific as decimal (have to install the library to have decimal)
+
+tip_percentage = 1/5 
+
+new_product = 150
+
