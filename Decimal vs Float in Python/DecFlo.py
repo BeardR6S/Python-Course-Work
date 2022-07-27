3d25e2e1041b8f0b8045d295d00a3ab833187f6f
BeardR6S <brandon.allan.hale@gmail.com>
2022-07-27 17:57:48 -0400

    More class parts

---
+++ Decimal vs Float in Python/DecFlo.py
@@ -0,0 +1,15 @@
+from decimal import Decimal #decimal is much more precise on the number
+
+# product_cost = 88.40
+# commision_rate = 0.08
+# qty = 450
+
+# product_cost +=(commision_rate * product_cost)
+# print(product_cost * qty) #42962.4
+
+product_cost = Decimal(88.40)
+commision_rate = Decimal(0.08)
+qty = 450
+
+product_cost +=(commision_rate * product_cost)
+print(product_cost * qty) #42962.40000000000282883716451
