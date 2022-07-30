0dc8fe0d3498778e87b625ba08ca2a41dc54ffb7
BeardR6S <brandon.allan.hale@gmail.com>
2022-07-30 16:13:13 -0400

    More Clarification

--- Join Function URL Query String/Practical.py
+++ Join Function URL Query String/Practical.py
@@ -2,7 +2,7 @@
 
 uri = 'https://www.google.com/search?q=' 
 tags = ['python', 'development', 'tutorial']  
-formatted_tags = '-'.join(tags) 
+formatted_tags = '-'.join(tags) #the item in the '' can be changed (commonly -, but can be anything)
 query_uir = f'{uri}{formatted_tags}'
 
 print(formatted_tags) #just the tags, no https://google.com...
