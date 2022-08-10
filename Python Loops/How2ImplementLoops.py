92c39c1fe98d26ccc664cdb1fb4b194ae6996ab3
BeardR6S <brandon.allan.hale@gmail.com>
2022-08-10 15:10:57 -0400

    .

--- Python Loops/How2ImplementLoops.py
+++ Python Loops/How2ImplementLoops.py
@@ -1,1 +1,19 @@
-players = ['Altuve', 'Bregman', 'Correa', 'Gattis']
+#Lists/Tuples Version of Loops
+
+# players = ['Altuve', 'Bregman', 'Correa', 'Gattis']
+
+# for player in players:
+#     print(player)
+
+
+#Dictionary Version of Loops
+players = {
+    '2B': 'Altuve',
+    '3B': 'Bregman',
+    'SS': 'Correa',
+    'DH': 'Gattis'
+}
+
+for position, player in players.items():
+    print('Position', position)
+    print('Player Name', player)
