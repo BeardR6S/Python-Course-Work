6d7712a8a6176f698809f2587abb1ed0add9eb99
BeardR6S <brandon.allan.hale@gmail.com>
2022-08-12 23:55:47 -0400

    ForInCombine

--- Python Looping/CombineFlattenListsForInLoop.py
+++ Python Looping/CombineFlattenListsForInLoop.py
@@ -1,3 +1,18 @@
+from types import new_class
+
+
 legacy_customers = ['Alice', 'Bob']
 new_customers = ['Tiffany', 'Kristine']
 
+# raw_db = [legacy_customers, new_customers]
+
+# print(raw_db)
+
+#What you get =  [['Alice', 'Bob'], ['Tiffany', 'Kristine']]
+
+#what we want = ['Alice', 'Bob', 'Tiffany', 'Kristine'] 
+
+for legacy_customer in legacy_customers:
+    new_customers.append(legacy_customer)
+
+print(new_customers)
